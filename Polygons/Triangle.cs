6cce82e83c118d3ab047f2b17ee093ef8819d38f
Ahmed Montasser <ahmed.sharkawy99@hotmail.com>
2023-12-10 16:31:48 +0200

    Adding Indexer

--- Polygons/Triangle.cs
+++ Polygons/Triangle.cs
@@ -8,7 +8,11 @@ namespace Polygons
     {
         private double[] sideLengths = new double[3];
 
-
+        public double this[int index]
+        {
+            get { return sideLengths[index]; }
+            set { sideLengths[index] = value; }
+        }
         protected Triangle(double s1, double s2, double s3, bool isRegular = false) : base(3, isRegular)
         {
             sideLengths[0] = s1;
@@ -16,6 +20,27 @@ namespace Polygons
             sideLengths[2] = s3;
         }
 
+        public static Triangle CreateTriangle(double s1, double s2, double s3)
+        {
+            if (s1 == s2 && s2 == s3)
+            {
+                return new EquilateralTriangle(s1);
+            }
+            else if (s1 == s3) {
+                return new IsoscelesTriangle(legs:s1, baseLength:s2);
+            }
+            else if (s2 == s3) {
+                return new IsoscelesTriangle(legs: s2, baseLength: s1);
+            }
+            else if (s1 == s2)
+            {
+                return new IsoscelesTriangle(legs: s1, baseLength: s3);
+            }
+            else
+            {
+                return new Triangle(s1, s2, s3);
+            }
+        }
         public override double Perimeter => sideLengths[0] + sideLengths[1] + sideLengths[2];
         public override double Area //Heron's Formula
         {
@@ -28,4 +53,5 @@ namespace Polygons
 
 
     }
+
 }
