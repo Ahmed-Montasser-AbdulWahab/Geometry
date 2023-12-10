5f25b1fa593a314fea2b95dcf36d026062a0b739
Ahmed Montasser <ahmed.sharkawy99@hotmail.com>
2023-12-10 13:54:12 +0200

    Add project files.

---
+++ Polygons/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Polygons
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
