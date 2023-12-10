26858a2da5de53c27ba2369d878f2bbe7f1960f8
Ahmed Montasser <ahmed.sharkawy99@hotmail.com>
2023-12-10 14:19:19 +0200

    Creating Square Class

--- Polygons/Square.cs
+++ Polygons/Square.cs
@@ -2,14 +2,26 @@
 {
     public class Square : Polygon
     {
+        private double _sideLength = 0;
 
+        public double SideLength
+        {
+            get => _sideLength;
+
+
+            set
+            {
+                if (value > 0)
+                    _sideLength = value;
+            }
+        }
 
         public Square(double sideLength) : base(4)
         {
-            
+            SideLength = sideLength;
         }
-        public override double Area => throw new System.NotImplementedException();
+        public override double Area => SideLength * SideLength;
 
-        public override double Perimeter => throw new System.NotImplementedException();
+        public override double Perimeter => SideLength * 4;
     }
 }
